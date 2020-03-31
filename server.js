b5e9cd2fdbf7356122cfc878999523fcf3ff135c
Esther Walker <equestriennesrule@yahoo.com>
2020-03-30 17:53:41 -0700

    got all my routes and seeds working

--- server.js
+++ server.js
@@ -3,13 +3,12 @@
 const express = require(`express`);
 const mongoose = require(`mongoose`);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static(`public`));
 
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budget`, {
@@ -20,5 +19,6 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budget`, {
 
 // routes
 app.use(require(`./routes/api.js`));
+app.use(require(`./routes/htmlroutes.js`));
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
